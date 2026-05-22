3e5c8b6925621ee3cc3c452eb049fd9f7da24d24
Kyle Gosselin-Harris <kyle.gosselinharris@gmail.com>
2026-05-22 11:10:42 -0700

    Release v0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.lnkranch.yaga"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
